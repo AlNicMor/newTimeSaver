e667f58a1651d37e0d5cea29804d66da15c21eea
amoreno <alan.moreno@quadminds.com>
2017-01-03 23:53:17 -0300

    added several new packages and tests

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import {FormControl} from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import Autosuggest from 'react-autosuggest';
+import Select from 'react-select';
+import createFilterOptions from 'react-select-fast-filter-options'
 import './App.css';
 
 
@@ -33,58 +36,71 @@ import './App.css';
 
 class App extends Component {
 
-constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state = {
-    result : null,
-  };
+    this.state = {
+      result: null,
+      filterOptions: null,
+    };
 
-  this.fetchTest = this.fetchTest.bind(this);
-  this.setResult = this.setResult.bind(this);
-}
+    this.fetchTest = this.fetchTest.bind(this);
+    this.setResult = this.setResult.bind(this);
+    this.logChange = this.logChange.bind(this);
+    this.setFilterOptions = this.setFilterOptions.bind(this);
+  }
 
-setResult(result){
-  this.setState({result});
-}
+  setResult(result) {
+    this.setState({ result });
+  }
 
-fetchTest(){
-axios.post('https://dev.quadminds.com/timeSaver.php', {
-    o: 'h',
-    u: '1316',
-    key: 'f76fa8021eac1dd37a004c84ac1e7705'
-})
-  .then(response => this.setResult(response))
-  .catch(function (error) {
-    console.log(error);
-  });
+  setFilterOptions(result){
+    const filterOptions = createFilterOptions({ result })
+    this.setState({filterOptions});
+  }
 
-}
+  logChange(val) {
+    console.log("Selected: " + val);
+  }
 
-componentDidMount(){
-  this.fetchTest();
-}
+  fetchTest() {
+    axios.get('https://dev.quadminds.com/timeSaver.php', {
+      params: {
+        action: 'getUserIdList',
+        key: '0195e692c9a946e1b59f383ec3844bcd'
+      }
+    })
+      .then(response => {
+        this.setResult(response.data);
+        this.setFilterOptions(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.fetchTest();
+  }
 
 
   render() {
     return (
- <div>
+      <div>
         <Grid>
           <Row>
             <Col md={6}>
               <h2>Heading</h2>
               <p>Adipisicing ratione incidunt eaque expedita rerum porro inventore. Nihil sit ipsam iure officiis sit eos at quibusdam natus dignissimos natus dolore! Vel doloremque ipsa alias nihil harum laborum necessitatibus rerum?</p>
-                        <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter text"
-            onChange={this.handleChange}
-          />
-            </Col>
-            <Col md={6}>
-              <h2>Heading</h2>
-              <p>Sit quia nemo quis enim provident porro eaque accusamus tenetur provident aliquid commodi? Velit nesciunt maiores obcaecati totam praesentium sint vitae exercitationem quaerat maxime iusto et! Consequatur aspernatur sit impedit.</p>
-              <p><Button>View details »</Button></p>
+                <Select
+                    filterOptions={this.state.filterOptions}
+                    name="form-field-name"
+                    value="one"
+                    options={this.state.result}
+                    onChange={this.logChange}
+                    valueKey='id'
+                    labelKey='id'
+                />
             </Col>
           </Row>
         </Grid>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'react-select/dist/react-select.css';
 import App from './App';
 import './index.css';
 
